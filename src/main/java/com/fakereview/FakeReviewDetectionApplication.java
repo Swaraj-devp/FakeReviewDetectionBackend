174062ed146c4943f961d6c5dea9f7093077bf5a
swarajghadage <148381059+swarajghadage@users.noreply.github.com>
2025-12-21 20:44:10 +0530

    Created new project and uploaded on github

---
+++ src/main/java/com/fakereview/FakeReviewDetectionApplication.java
@@ -0,0 +1,12 @@
+package com.fakereview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakeReviewDetectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FakeReviewDetectionApplication.class, args);
+	}
+}
